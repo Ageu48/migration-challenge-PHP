bf63b62c90a8318b845b23ad7020807ebbb46239
Ageu48 <ageuoliveira48@gmail.com>
2023-11-17 18:10:45 -0400

    apenas mais um

--- lib.php
+++ lib.php
@@ -4,7 +4,77 @@
     Você pode separar funções muito utilizadas nesta biblioteca, evitando replicação de código.
 */
 
-function dateNow(){
+function dateNow()
+{
   date_default_timezone_set('America/Sao_Paulo');
   return date('d-m-Y \à\s H:i:s');
+}
+
+function lerArquivosCSV($caminhoPasta)
+{
+  // Verifica se o caminho da pasta é válido
+  if (!is_dir($caminhoPasta)) {
+    die("Caminho da pasta inválido");
+  }
+
+  // Inicializa um array para armazenar os dados dos arquivos CSV
+  $dadosCSV = array();
+
+  // Obtém a lista de arquivos na pasta
+  $arquivos = scandir($caminhoPasta);
+
+  // Filtra apenas os arquivos CSV
+  $arquivosCSV = array_filter($arquivos, function ($arquivo) {
+    return pathinfo($arquivo, PATHINFO_EXTENSION) === 'csv';
+  });
+
+  // Loop pelos arquivos CSV
+  foreach ($arquivosCSV as $arquivo) {
+    $caminhoCompleto = $caminhoPasta . '/' . $arquivo;
+
+    // Verifica se o arquivo é legível
+    if (is_readable($caminhoCompleto)) {
+      // Lê o conteúdo do arquivo CSV
+      $conteudoCSV = file($caminhoCompleto);
+
+      // Inicializa um array para armazenar os dados do arquivo CSV
+      $dadosArquivo = array();
+
+      // Itera sobre as linhas do arquivo CSV
+      foreach ($conteudoCSV as $linha) {
+        $dados = str_getcsv($linha, ";");
+        $dadosArquivo[] = $dados;
+      }
+
+      // Armazena os dados do arquivo no array principal
+      $dadosCSV[$arquivo] = $dadosArquivo;
+    } else {
+      echo "Não foi possível ler o arquivo $arquivo.<br>";
+    }
+  }
+
+  // Retorna os dados CSV
+  return $dadosCSV;
+}
+
+function verificaEInsereConvenio($conexao, $id_convenio, $nome_convenio)
+{
+  $queryConvenioCheck = "SELECT id FROM convenios WHERE id = '$id_convenio'";
+  $resultConvenioCheck = mysqli_query($conexao, $queryConvenioCheck);
+
+  if (mysqli_num_rows($resultConvenioCheck) == 0) {
+    $queryConvenioInsert = "INSERT INTO convenios (id, nome) VALUES ('$id_convenio', '$nome_convenio')";
+    mysqli_query($conexao, $queryConvenioInsert);
+  }
+}
+
+function verificaEInsereProfissional($conexao, $id_profissional, $nome_profissional)
+{
+  $queryProfissionalCheck = "SELECT id FROM profissionais WHERE id = '$id_profissional'";
+  $resultProfissionalCheck = mysqli_query($conexao, $queryProfissionalCheck);
+
+  if (mysqli_num_rows($resultProfissionalCheck) == 0) {
+    $queryProfissionalInsert = "INSERT INTO profissionais (id, nome) VALUES ('$id_profissional', '$nome_profissional')";
+    mysqli_query($conexao, $queryProfissionalInsert);
+  }
 }
--- migrate.php
+++ migrate.php
@@ -14,20 +14,69 @@
 include "./lib.php";
 
 // Conexão com o banco da clínica fictícia:
-$connMedical = mysqli_connect("localhost", "root", "root", "MedicalChallenge")
+$connMedical = mysqli_connect("localhost", "root", "", "MedicalChallenge")
   or die("Não foi possível conectar os servidor MySQL: MedicalChallenge\n");
 
 // Conexão com o banco temporário:
-$connTemp = mysqli_connect("localhost", "root", "root", "0temp")
+$connTemp = mysqli_connect("localhost", "root", "", "0temp")
   or die("Não foi possível conectar os servidor MySQL: 0temp\n");
 
 // Informações de Inicio da Migração:
 echo "Início da Migração: " . dateNow() . ".\n\n";
 
+$caminhoPasta = 'dados_sistema_legado';
+$dados = lerArquivosCSV($caminhoPasta);
 
-/*
-  Seu código vai aqui!
-*/
+foreach ($dados as $nomeArquivo => $dadosArquivo) {
+  // Ignora a primeira linha (cabeçalho) do arquivo CSV
+  array_shift($dadosArquivo);
+
+  foreach ($dadosArquivo as $linha) {
+    if ($nomeArquivo == 'pacientes.csv') {
+      $id = $linha[0];
+      $nome = $linha[1];
+      $sexo = $linha[7];
+      $nascimento = date('Y-m-d', strtotime($linha[2])); // Formata a data para o formato MySQL
+      $cpf = $linha[5];
+      $rg = $linha[6];
+      $id_convenio = $linha[8];
+      $nome_convenio = $linha[9];
+
+      // Verifica e insere na tabela convenios se necessário
+      verificaEInsereConvenio($connTemp, $id_convenio, $nome_convenio);
+
+      // Monta a query de inserção para a tabela pacientes
+      $query = "INSERT INTO pacientes (id, nome, sexo, nascimento, cpf, rg, id_convenio) VALUES ('$id', '$nome', '$sexo', '$nascimento', '$cpf', '$rg', '$id_convenio')";
+    } elseif ($nomeArquivo == 'agendamentos.csv') {
+      $id = $linha[0];
+      $id_paciente = $linha[5];
+      $id_profissional = $linha[7];
+      $nome_profissional = $linha[8];
+
+      // Verifica e insere na tabela profissionais se necessário
+      verificaEInsereProfissional($connTemp, $id_profissional, $nome_profissional);
+
+      $id_convenio = $linha[9];
+      $nome_convenio = $linha[10];
+
+      // Verifica e insere na tabela convenios se necessário
+      verificaEInsereConvenio($connTemp, $id_convenio, $nome_convenio);
+
+      $dh_inicio = date('Y-m-d H:i:s', strtotime($linha[2] . ' ' . $linha[3]));
+      $dh_fim = date('Y-m-d H:i:s', strtotime($linha[2] . ' ' . $linha[4]));
+      $observacoes = $linha[1];
+
+      // Monta a query de inserção para a tabela agendamento
+      $query = "INSERT INTO agendamentos (id, id_profissional, dh_inicio, dh_fim, id_convenio, observacoes) VALUES ('$id', '$id_profissional', '$dh_inicio', '$dh_fim', '$id_convenio', '$observacoes')";
+    }
+
+    // Executa a query
+    mysqli_query($connTemp, $query);
+
+    // Atraso de 2 segundos
+    //sleep(2);
+  }
+}
 
 
 // Encerrando as conexões:
@@ -36,4 +85,3 @@ $connTemp->close();
 
 // Informações de Fim da Migração:
 echo "Fim da Migração: " . dateNow() . ".\n";
-
